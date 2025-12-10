4e7b25e75ba4e87bb3a1cd575c33d928d5afd718
Maja Sundin <m.sundinn@hotmail.com>
2025-12-10 17:35:41 +0100

    Add clickable logo and improve header layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,7 @@
-import { Burger, Container, Group } from "@mantine/core";
+import { Container, Group, ActionIcon, Indicator } from "@mantine/core";
 import classes from "./header.module.css";
-import { ActionIcon } from "@mantine/core";
 import { IconShoppingCart } from "@tabler/icons-react";
-import { Indicator } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/CartContext";
 
@@ -14,9 +11,8 @@ const links = [
 ];
 
 export const Header = () => {
-  const [opened, { toggle }] = useDisclosure(false);
-
   const { cart } = useContext(CartContext);
+  const location = useLocation();
 
   const totalQuantity = cart.items.reduce(
     (sum, item) => sum + item.quantity,
@@ -24,38 +20,78 @@ export const Header = () => {
   );
 
   const items = links.map((link) => (
-    <Link key={link.label} to={link.link} className={classes.link}>
+    <Link
+      key={link.label}
+      to={link.link}
+      className={
+        location.pathname === link.link
+          ? `${classes.link} ${classes.active}`
+          : classes.link
+      }
+    >
       {link.label}
     </Link>
   ));
+
   return (
     <header className={classes.header}>
       <Container size="md" className={classes.inner}>
-        <Group>
-          <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
-          <img src="/logoo.png" alt="logo" style={{ height: "50px" }} />
+        <Group justify="space-between" align="center">
+          <Link to="/">
+            <img
+              src="/logoo.png"
+              alt="logo"
+              style={{ height: "50px", cursor: "pointer" }}
+            />
+          </Link>
+
+          <Group gap={5} visibleFrom="sm">
+            {items}
+            <Indicator
+              color="darkgreen"
+              label={totalQuantity}
+              size={16}
+              disabled={totalQuantity === 0}
+            >
+              <ActionIcon
+                component={Link}
+                to="/cart"
+                color="darkgreen"
+                variant="subtle"
+                aria-label="Varukorg"
+              >
+                <IconShoppingCart />
+              </ActionIcon>
+            </Indicator>
+          </Group>
+
+          <Group hiddenFrom="sm">
+            <Indicator
+              color="darkgreen"
+              label={totalQuantity}
+              size={16}
+              disabled={totalQuantity === 0}
+            >
+              <ActionIcon
+                component={Link}
+                to="/cart"
+                color="darkgreen"
+                variant="subtle"
+                aria-label="Varukorg"
+              >
+                <IconShoppingCart />
+              </ActionIcon>
+            </Indicator>
+          </Group>
         </Group>
 
-        <Group>
-          <Group ml={50} gap={5} className={classes.links} visibleFrom="sm">
-            {items}
-          </Group>
-          <Indicator
-            color="darkgreen"
-            label={totalQuantity}
-            size={16}
-            disabled={totalQuantity === 0}
-          >
-            <ActionIcon
-              component={Link}
-              to="/cart"
-              color="darkgreen"
-              variant="subtle"
-              aria-label="Varukorg"
-            >
-              <IconShoppingCart />
-            </ActionIcon>
-          </Indicator>
+        <Group
+          justify="center"
+          gap={0}
+          className={classes.navMobile}
+          hiddenFrom="sm"
+        >
+          {items}
         </Group>
       </Container>
     </header>
